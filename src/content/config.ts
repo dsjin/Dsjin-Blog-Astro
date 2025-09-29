771823b6fce06b10194f0203be08952d5eb2d1ad
Thatchakon Jom-ud <dsjinj@gmail.com>
2025-09-29 18:27:41 +0900

    added post page

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,11 +2,11 @@ import { z } from 'zod'
 import { defineCollection } from 'astro:content'
 
 const blogCollection = defineCollection({
-  schema: z.object({
+  schema: ({image}) => z.object({
     title: z.string(),
     description: z.string(),
     featured: z.boolean(),
-    coverImage: z.string().optional(),
+    coverImage: image().optional(),
     tags: z.array(z.string()),
     author: z.object({
       profileImage: z.string().optional(),
